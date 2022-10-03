5ea59d315197ec229643bd8d2196eaed12025d7a
Esequiel Morales <banzai@MacBook-Pro-de-Esequiel.local>
2022-10-03 17:04:46 +0200

    version 2.1

--- src/custom.js
+++ src/custom.js
@@ -6,9 +6,7 @@ window.onscroll = () => {
     let pos = window.pageYOffset;
     if (pos >= 10) {
         navbar[0].classList.add("navScrolled")
-        console.log("asd")
     } else {
         navbar[0].classList.remove("navScrolled")
-        console.log("as2d")
     }
 }
