68d38c97fa59dff8bae477e7a9ccc64f2a8c4420
Esequiel Morales <banzai@MBP-de-Esequiel.home>
2022-10-06 17:35:21 +0200

    version 2.4

--- src/profile.js
+++ src/profile.js
@@ -1,5 +1,14 @@
 import { Container, Row, Col } from "react-bootstrap";
 import profilePicture from "../src/img/profile.png"
+import htmlIcon from "../src/img/icons/html-5.png"
+import cssIcon from "../src/img/icons/css-3.png"
+import bootstrapIcon from "../src/img/icons/bootstrap.png"
+import javascriptIcon from "../src/img/icons/js.png"
+import jqueryIcon from "../src/img/icons/jquery.png"
+import reactIcon from "../src/img/icons/atoms.png"
+import sqlIcon from "../src/img/icons/sql-server.png"
+import gitIcon from "../src/img/icons/git.png"
+import appscriptIcon from "../src/img/icons/google-apps-script.png"
 
 const ProfileComp = () =>
     <Container className="my-5 py-4">
@@ -22,15 +31,41 @@ const ProfileComp = () =>
             <Col xs="12" className="my-5">
                 <h4>Algunas de mis habilidades</h4>
             </Col>
-            <Col xs="12" md="2" className="d-flex justify-content-center align-items-center my-3 "><span className="skillTag">HTML</span></Col>
-            <Col xs="12" md="2" className="d-flex justify-content-center align-items-center my-3 "><span className="skillTag">CSS</span></Col>
-            <Col xs="12" md="2" className="d-flex justify-content-center align-items-center my-3 "><span className="skillTag">Bootstrap</span></Col>
-            <Col xs="12" md="2" className="d-flex justify-content-center align-items-center my-3 "><span className="skillTag">JavaScript</span></Col>
-            <Col xs="12" md="2" className="d-flex justify-content-center align-items-center my-3 "><span className="skillTag">Jquery</span></Col>
-            <Col xs="12" md="2" className="d-flex justify-content-center align-items-center my-3 "><span className="skillTag text-center">Learning: React</span></Col>
-            <Col xs="12" md="4" className="d-flex justify-content-center align-items-center my-3 "><span className="skillTag text-center">SQL</span></Col>
-            <Col xs="12" md="4" className="d-flex justify-content-center align-items-center my-3 "><span className="skillTag text-center">Git</span></Col>
-            <Col xs="12" md="4" className="d-flex justify-content-center align-items-center my-3 "><span className="skillTag text-center">AppScript</span></Col>
+            <Col xs="12" sm="6" md="2" className="d-flex justify-content-center align-items-center flex-column my-4 offset-md-1">
+                <img className="iconSkill" src={htmlIcon}></img>
+                <span className="skillTag">HTML</span>
+            </Col>
+            <Col xs="12" sm="6" md="2" className="d-flex justify-content-center align-items-center flex-column my-4 ">
+                <img className="iconSkill" src={cssIcon}></img>
+                <span className="skillTag">CSS</span>
+            </Col>
+            <Col xs="12" sm="6" md="2" className="d-flex justify-content-center align-items-center flex-column my-4 ">
+                <img className="iconSkill" src={bootstrapIcon}></img>
+                <span className="skillTag">Bootstrap</span>
+            </Col>
+            <Col xs="12" sm="6" md="2" className="d-flex justify-content-center align-items-center flex-column my-4 ">
+                <img className="iconSkill" src={javascriptIcon}></img>
+                <span className="skillTag">JavaScript</span>
+            </Col>
+            <Col xs="12" sm="6" md="2" className="d-flex justify-content-center align-items-center flex-column my-4 ">
+                <img className="iconSkill" src={jqueryIcon}></img>
+                <span className="skillTag">Jquery</span>
+            </Col>
+            <Col xs="12" sm="6" md="3" className="d-flex justify-content-center align-items-center flex-column my-4 ">
+                <img className="iconSkill" src={reactIcon}></img>
+                <span className="skillTag text-center">Learning: React</span>
+            </Col>
+            <Col xs="12" sm="6" md="3" className="d-flex justify-content-center align-items-center flex-column my-4 ">
+                <img className="iconSkill" src={sqlIcon}></img>
+                <span className="skillTag text-center">SQL</span>
+            </Col>
+            <Col xs="12" sm="6" md="3" className="d-flex justify-content-center align-items-center flex-column my-4 ">
+                <img className="iconSkill" src={gitIcon}></img>
+                <span className="skillTag text-center">Git</span>
+            </Col>
+            <Col xs="12" sm="6" md="3" className="d-flex justify-content-center align-items-center flex-column my-4 ">
+                <img className="iconSkill" src={appscriptIcon}></img>
+                <span className="skillTag text-center">AppScript</span></Col>
         </Row>
     </Container>
 
